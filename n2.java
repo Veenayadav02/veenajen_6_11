a9e433026b017798d879fde75e7f06173a5bbae5
nisarga <nisarga@gmail.com>
2023-11-06 04:15:57 +0000

    nisarga files

---
+++ n2.java
@@ -0,0 +1,22 @@
+class n2
+{
+	public static void main(String[] args) {
+		byte a = 10;
+		short b = 100;
+		int c = 1000;
+		long d = 1000;
+		float e = 10000.78f;
+		double f = 100000.78;
+		char g = 'B';
+		boolean h = false;
+		String i = "veena";
+		System.out.println("byte    :" +a);
+		System.out.println("short   :" +b);
+		System.out.println("int     :" +c);
+		System.out.println("long    :" +d);
+		System.out.println("double  :" +e);
+		System.out.println("char    :" +g);
+		System.out.println("boolean :" +h);
+		System.out.println("String  :"+i);
+	}
+}
