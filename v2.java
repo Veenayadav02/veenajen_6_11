813fdc6893e03bea63afe32936cbb4267a276568
veena <veena@gmail.com>
2023-11-06 04:07:09 +0000

    vee files

---
+++ v2.java
@@ -0,0 +1,15 @@
+class v2
+{
+	public static void main(String[] args) {
+		for (int i=50;i>=40;i--)
+			if(i%2==0)
+		 {
+			System.out.println("even -->"+i);
+			
+		}
+		else
+		{
+			System.out.println("odd -->"+i);
+		}
+	}
+}
